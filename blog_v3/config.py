e65ed289c77853966d6cb81855c78d0decc76e0f
aga <agatonovic7@gmail.com>
2024-04-22 16:56:50 +0200

    update config.py

--- blog_v3/config.py
+++ blog_v3/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 class Config:
     load_dotenv()
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DB_PATH = os.path.join(ROOT_DIR, 'blog_v3_database.db')
+    # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    # DB_PATH = os.path.join(ROOT_DIR, 'blog_v3_database.db')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_PATH
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URI')
 
     CKEDITOR_PKG_TYPE = 'basic'
